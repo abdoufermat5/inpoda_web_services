6348c4d803def939475e3827ebbe20cc7b5f90a9
abdoufermat5 <saabdouyaya@ept.sn>
2023-02-19 01:09:27 +0100

    creds temp

--- serviceApp/db/dbInstance.py
+++ serviceApp/db/dbInstance.py
@@ -3,17 +3,21 @@ from spyne import Unicode, TTableModel, UnsignedInteger32
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('spyne.protocol.xml').setLevel(logging.DEBUG)
 logging.getLogger('sqlalchemy.engine.base.Engine').setLevel(logging.DEBUG)
 
-dbname = 'postgres'
-user = 'abdoufermat@inpodadb'
-host = 'inpodadb.postgres.database.azure.com'
-password = 'Fanta1976'
-port = '5432'
-sslmode = 'true'
+load_dotenv()
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+port = os.getenv('DB_PORT')
+dbname = os.getenv('DB_DATABASE')
+
 
 db = create_engine("postgresql://{}:{}@{}:{}/{}".format(user, password, host, port, dbname))
 Session = sessionmaker(bind=db)
